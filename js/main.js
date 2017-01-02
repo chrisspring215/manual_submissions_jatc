c8987759dbc4e6b317f456b206ca81d316023223
Christopher Collins <chris@spring215.com>
2017-01-02 08:20:49 -0500

    more validation on first form

--- js/main.js
+++ js/main.js
@@ -31,6 +31,51 @@ function checkStudentId() {
 	}
 }
 
+// Check not selected on course
+function checkCourse() {
+	var courseChoice = document.getElementById('courseChoice').value;
+
+
+	if (courseChoice === 'Choose One') {
+		document.getElementById('courseError').setAttribute('class', 'error');
+		document.getElementById('courseError').innerHTML = " Choose a course";
+	}
+
+	else {
+		document.getElementById('courseError').setAttribute('class', 'hidden');
+	}
+}
+
+// Check not selected on submission type
+function checkSubmissionType() {
+	var submissionChoice = document.getElementById('submissionChoice').value;
+
+
+	if (submissionChoice === 'Choose One') {
+		document.getElementById('submissionError').setAttribute('class', 'error');
+		document.getElementById('submissionError').innerHTML = " Choose a submission type";
+	}
+
+	else {
+		document.getElementById('submissionError').setAttribute('class', 'hidden');
+	}
+}
+
+// Check not selected on lesson
+function checkLesson() {
+	var lessonChoice = document.getElementById('lessonChoice').value;
+
+
+	if (lessonChoice === 'Choose One') {
+		document.getElementById('lessonError').setAttribute('class', 'error');
+		document.getElementById('lessonError').innerHTML = " Choose a lesson";
+	}
+
+	else {
+		document.getElementById('lessonError').setAttribute('class', 'hidden');
+	}
+}
+
 // Check for bad string while entering Attempt ID
 function attemptIdKeytest() {
 	var attemptId = document.getElementById('attemptId').value;
@@ -156,4 +201,91 @@ function checkAnswerResponse() {
     	}
 	}
 	tally();
+}
+
+
+// Check for bad string while entering date
+function dateKeyTest() {
+	var monthDate = document.getElementById('month').value;
+	var monthDateNumber = Number(document.getElementById('month').value);
+
+	var dayDate = document.getElementById('day').value;
+	var dayDateNumber = Number(document.getElementById('day').value);
+
+	var yearDate = document.getElementById('year').value;
+	var yearDateNumber = Number(document.getElementById('year').value);
+
+	// Checks month for errors
+	if (monthDate.match(/[a-z]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Can not use letters"
+	}
+
+	else if (monthDate.length != 2) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Use two digit month format: Jan = 01"
+	}
+
+	else if (monthDateNumber > 12) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Not a month: 01-12"
+	}
+
+	else if (monthDate.match(/[0-9]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'hidden');
+	}
+
+	// Checks day for errors
+	if (dayDate.match(/[a-z]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Can not use letters"
+	}
+
+	else if (dayDate.length != 2) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Use two digit day format: 1st = 01"
+	}
+
+	else if (dayDateNumber > 31) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Not a day: 01-31"
+	}
+
+	else if (dayDate.match(/[0-9]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'hidden');
+	}
+
+	// Checks year for errors
+	if (yearDate.match(/[a-z]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Can not use letters"
+	}
+
+	else if (yearDate.length != 4) {
+    document.getElementById('submissionDateError').setAttribute('class', 'error');
+    document.getElementById('submissionDateError').innerHTML = " Use four digit year format: 2016, 2020"
+	}
+
+	else if (yearDate.match(/[0-9]/i)) {
+    document.getElementById('submissionDateError').setAttribute('class', 'hidden');
+	}
+}
+
+// Check for bad string when Student ID loses focus
+function checkStudentId() {
+	var studentId = document.getElementById('studentId').value;
+
+	if (studentId.length != 9) {
+		document.getElementById('studentIdError').setAttribute('class', 'error');
+		document.getElementById('studentIdError').innerHTML = " Enter '@' followed by 8 digits";
+	}
+
+	else if (studentId.match(/[a-z]/i)) {
+    document.getElementById('studentIdError').setAttribute('class', 'error');
+    document.getElementById('studentIdError').innerHTML = " Can not use letters"
+	}
+
+	else{
+		document.getElementById('studentIdError').setAttribute('class', 'hidden')
+	}
 }
